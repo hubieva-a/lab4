81a58d3a0baeb07748f6aef51f69afff00c60b59
hubieva-a <72037369+hubieva-a@users.noreply.github.com>
2021-01-08 23:18:46 +0300

    Add files via upload
    Задания 1, 2 и 3

---
+++ 1.py
@@ -0,0 +1,22 @@
+import sys
+if __name__ == '__main__':
+    n = input("Number of the day of the week")
+    n = int(n)
+
+    if n == 1:
+        print("Monday")
+    elif n== 2:
+        print("Tuesday")
+    elif n == 3:
+        print("Wednesday")
+    elif n == 4:
+        print("Thursday")
+    elif n == 5:
+        print("Friday")
+    elif n == 6:
+        print("Saturday")
+    elif n == 7:
+        print("Sunday")
+    else:
+        print("Please choose a number from 1 to 7")
+        exit(1)
---
+++ 2.py
@@ -0,0 +1,18 @@
+a = int(input("A = "))
+b = int(input("B = "))
+c = int(input("C = "))
+
+if a > b and a > c:
+    if b > c:
+        print(c, b, a)
+        print(a, b, c)
+    else:
+        print(b, c, a)
+        print(a, c, b)
+elif a > b and c> a:
+    print(b, a, c)
+    print(c, a, b)
+elif b > a and c > a and c > b:
+    print(a, b, c)
+    print(c, b, a)
+    exit(1)
